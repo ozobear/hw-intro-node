c4be08f72cd6a1abe74e70c7be2ab6db601fbe87
ozobear <ozober@gmail.com>
2019-01-24 14:31:43 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,38 @@
 class SortedList {
   constructor() {
-    
+    this.items = [];
+    this.length = 0;
   }
-  add(item) {
-    
+  add(x) {
+   this.items.push(x);
+   this.length++;
+   this.items.sort((a, b) => a - b);
   }
   get(pos) {
-  
+    return this.items[pos-1];
   }
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+  min() {
+    return(this.length === 0 ? (() => {
+      throw new Error("EmptySortedList")
+    })() : Math.min(...this.items));
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum()/this.length;
+    }
+  }
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
 };
 
 module.exports = SortedList;
